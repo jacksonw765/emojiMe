3e431bd4e2a543691dc33a7c52cd0b477fdfcd79
Jackson OldDesktop <jacksonw765@gmail.com>
2020-10-26 22:03:25 -0400

    some updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
-import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:dotted_border/dotted_border.dart';
+import 'package:emojieme/chars.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_xlider/flutter_xlider.dart';
 import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
+import 'package:ndialog/ndialog.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:pinch_zoom/pinch_zoom.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+import 'alerts.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,8 +44,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   img.Image imagePath;
+  img.Image orgImage;
   String dir;
   PanelController controller = PanelController();
+  Alerts alerts = Alerts();
   static BoxShadow shadow2 = BoxShadow(spreadRadius: -20, color: Colors.black45, blurRadius: 20, offset: Offset(0, 14));
 
   @override
@@ -55,66 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: const EdgeInsets.only(top: 35),
             child: Align(
               alignment: Alignment.topCenter,
-              child: DottedBorder(
-                color: Colors.black45,
-                borderType: BorderType.RRect,
-                radius: Radius.circular(20),
-                child: Container(
-                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(20))),
-                  height: 475,
-                  width: 350,
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 10.0, left: 15),
-                          child: Text(
-                            'Import Media',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                            ),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 75.0),
-                        child: Container(
-                          child: Column(
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.add_a_photo_outlined),
-                                onPressed: () {
-
-                                },
-                                iconSize: 96,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 25),
-                                child: Text(
-                                  'Or',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 18,
-                                  ),
-                                  textAlign: TextAlign.left,
-                                ),
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.photo_library_outlined),
-                                onPressed: null,
-                                iconSize: 96,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              child: buildImage(),
             ),
           ),
           panel: Align(
@@ -122,7 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: FlatButton(
               minWidth: 25,
               onPressed: null,
-              //color: Colors.blue,
               child: Column(children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 0.0),
@@ -134,7 +80,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           flex: 54,
                         ),
                         Container(
-                          //color: Colors.black45,
                           width: 50,
                           height: 7,
                           decoration: BoxDecoration(color: Colors.black38, borderRadius: BorderRadius.all(Radius.circular(20))),
@@ -325,14 +270,17 @@ class _MyHomePageState extends State<MyHomePage> {
                               flex: 4,
                             ),
                             Text(
-                              ':)',
+                              Char.selectedChar,
                               style: TextStyle(fontSize: 20),
                             ),
                             Spacer(
                               flex: 10,
                             ),
                             FlatButton(
-                              onPressed: null,
+                              onPressed: () {
+                                showCharList(context);
+                                setState(() {});
+                              },
                               child: Text(
                                 'Select',
                                 style: TextStyle(color: Colors.blue, fontSize: 18),
@@ -353,21 +301,90 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget getImage() {
-    if (imagePath != null) {
-      //File('$dir/tmp.png').writeAsBytesSync(img.encodePng(imagePath));
-      return Container(
-        height: 500,
-        child: PinchZoom(
-          image: Image.memory(img.encodePng(imagePath)),
-          zoomedBackgroundColor: Colors.transparent,
-          resetDuration: const Duration(milliseconds: 100),
-          //maxScale: 15.0,
-        ),
-      );
-    } else {
-      return Text('');
+  void showCharList(BuildContext context) async {
+    List<Widget> buildRows() {
+      List<Widget> retval = [];
+      for (int x = 0; x < Char.CHARS.length; x += 4) {
+        if (Char.CHARS.length > x + 3) {
+          retval.add(Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CharWidget3(Char.CHARS[x]),
+              CharWidget3(Char.CHARS[x + 1]),
+              CharWidget3(Char.CHARS[x + 2]),
+              CharWidget3(Char.CHARS[x + 3]),
+            ],
+          ));
+        } else if (Char.CHARS.length > x + 2) {
+          retval.add(Row(
+            children: [
+              CharWidget3(Char.CHARS[x]),
+              CharWidget3(Char.CHARS[x + 1]),
+              CharWidget3(Char.CHARS[x + 2]),
+            ],
+          ));
+        } else if (Char.CHARS.length > x + 1) {
+          retval.add(Row(
+            children: [
+              CharWidget3(Char.CHARS[x]),
+              CharWidget3(Char.CHARS[x + 1]),
+            ],
+          ));
+        } else {
+          retval.add(Row(
+            children: [
+              CharWidget3(Char.CHARS[x]),
+            ],
+          ));
+        }
+      }
+      return retval;
     }
+
+    await NDialog(
+      dialogStyle: DialogStyle(
+        titleDivider: true,
+      ),
+      title: Text("Select Emoji"),
+      content: Container(
+        height: 450,
+        width: 450,
+        child: ListView(
+          children: buildRows(),
+        ),
+      ),
+      //actions: <Widget>[],
+    ).show(context);
+  }
+
+  Widget CharWidget3(String char) {
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 85,
+          width: 60,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            boxShadow: [shadow2],
+            color: Colors.white,
+          ),
+          child: Center(
+            child: Text(
+              char,
+              style: TextStyle(fontSize: 32, color: Colors.black),
+            ),
+          ),
+        ),
+      ),
+      onTap: () {
+        print('here');
+          Char.selectedChar = char;
+          setState(() {});
+
+        //Navigator.pop(context);
+      },
+    );
   }
 
   void perm() async {
@@ -403,5 +420,110 @@ class _MyHomePageState extends State<MyHomePage> {
       imagePath = image2;
     });
     print('Done');
+  }
+
+  Widget buildImage() {
+    if (orgImage != null) {
+      return Stack(children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: IconButton(
+                icon: Icon(
+                  Icons.remove_circle,
+                  color: Colors.red,
+                ),
+                onPressed: () {
+                  setState(() {
+                    orgImage = null;
+                  });
+                }),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              //height: 500,
+              constraints: BoxConstraints(
+                  //maxHeight: 600,
+                  //maxWidth: 400,
+                  ),
+              child: Transform.rotate(angle: pi / 2, child: Image.memory(img.encodePng(orgImage))),
+            ),
+          ),
+        )
+      ]);
+    } else {
+      return DottedBorder(
+        color: Colors.black45,
+        borderType: BorderType.RRect,
+        radius: Radius.circular(20),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(20))),
+          height: 475,
+          width: 350,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0, left: 15),
+                  child: Text(
+                    'Import Media',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 75.0),
+                child: Container(
+                  child: Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.add_a_photo_outlined),
+                        onPressed: () async {
+                          await [Permission.camera].request();
+                          alerts.showLoading(context);
+                          PickedFile imagePick = await ImagePicker.platform.pickImage(source: ImageSource.camera);
+                          Uint8List imageBytes = await imagePick.readAsBytes();
+                          alerts.dismissContext();
+                          setState(() {
+                            this.orgImage = img.decodeImage(imageBytes);
+                          });
+                        },
+                        iconSize: 96,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Text(
+                          'Or',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.photo_library_outlined),
+                        onPressed: null,
+                        iconSize: 96,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
